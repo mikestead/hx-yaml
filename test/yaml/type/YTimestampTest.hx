796d0c90bf9b16ff92908ecf22266862f9f2d0bb
Mike Stead <stead.mike@gmail.com>
2014-04-17 17:18:06 +1000

    Add support for CPP. Requires Haxe 3.1.3+

--- test/yaml/type/YTimestampTest.hx
+++ test/yaml/type/YTimestampTest.hx
@@ -4,7 +4,7 @@ import massive.munit.Assert;
 
 class YTimestampTest
 {
-	static inline var STAMP:Float = 1008385183100;
+	static var STAMP:Float = 1008385183100;
 
 	var type:YTimestamp;
 
@@ -25,12 +25,13 @@ class YTimestampTest
 		var spaceSeparated = "2001-12-14 21:59:43.10 -5";
 		var noTimeZone = "2001-12-15 2:59:43.10";
 		var date = "2002-12-14";
+		var time:Float = 1039824000000;
 		
 		Assert.areEqual(STAMP, type.resolve(canonical).getTime());
 		Assert.areEqual(STAMP, type.resolve(validIso8601).getTime());
 		Assert.areEqual(STAMP, type.resolve(spaceSeparated).getTime());
 		Assert.areEqual(STAMP, type.resolve(noTimeZone).getTime());
-		Assert.areEqual(1039824000000, type.resolve(date).getTime());
+		Assert.areEqual(time, type.resolve(date).getTime());
 	}
 
 	#if !(js || flash)
