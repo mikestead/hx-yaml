3a840f38d144e211d4161d5007f32c06d66daf07
Mike Stead <mike@massive.co>
2020-02-21 12:11:08 +1300

    suppress Utf8 deprecation warning

--- test/TestSuite.hx
+++ test/TestSuite.hx
@@ -1,39 +1,37 @@
 import massive.munit.TestSuite;
 
-import yaml.type.YBinaryTest;
-import yaml.type.YBoolTest;
-import yaml.type.YFloatTest;
-import yaml.type.YIntTest;
-import yaml.type.YMergeTest;
-import yaml.type.YNullTest;
-import yaml.type.YOmapTest;
-import yaml.type.YPairsTest;
+import yaml.YamlTest;
 import yaml.type.YSetTest;
 import yaml.type.YTimestampTest;
-import yaml.YamlTest;
+import yaml.type.YNullTest;
+import yaml.type.YMergeTest;
+import yaml.type.YIntTest;
+import yaml.type.YBoolTest;
+import yaml.type.YBinaryTest;
+import yaml.type.YPairsTest;
+import yaml.type.YFloatTest;
+import yaml.type.YOmapTest;
 
 /**
  * Auto generated Test Suite for MassiveUnit.
  * Refer to munit command line tool for more information (haxelib run munit)
  */
-
 class TestSuite extends massive.munit.TestSuite
-{		
-
+{
 	public function new()
 	{
 		super();
 
-		add(yaml.type.YBinaryTest);
-		add(yaml.type.YBoolTest);
-		add(yaml.type.YFloatTest);
-		add(yaml.type.YIntTest);
-		add(yaml.type.YMergeTest);
-		add(yaml.type.YNullTest);
-		add(yaml.type.YOmapTest);
-		add(yaml.type.YPairsTest);
+		add(yaml.YamlTest);
 		add(yaml.type.YSetTest);
 		add(yaml.type.YTimestampTest);
-		add(yaml.YamlTest);
+		add(yaml.type.YNullTest);
+		add(yaml.type.YMergeTest);
+		add(yaml.type.YIntTest);
+		add(yaml.type.YBoolTest);
+		add(yaml.type.YBinaryTest);
+		add(yaml.type.YPairsTest);
+		add(yaml.type.YFloatTest);
+		add(yaml.type.YOmapTest);
 	}
 }
